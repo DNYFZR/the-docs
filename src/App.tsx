dfa9c75cad4c28a42129ebea6b23af0867e5e71a
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-05-29 09:26:55 +0100

    Updated app

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,25 @@ import DisplayResult from "./components/SearchData";
 
 import data_ai from "./database/ai.json";
 import data_api from "./database/api.json";
-import data_blog from "./database/blogs.json";
-import data_books from "./database/books.json";
 import data_code from "./database/code.json";
 import data_devops from "./database/devops.json";
 import data_swe from "./database/swe.json";
+import data_read from "./database/reading.json"
 
 const pageMap: SwitchMap = {
   "AI": () => <DisplayResult data={data_ai} />,
   "APIs" :() => <DisplayResult data={data_api} />,
-  "Blogs": () => <DisplayResult data={data_blog} />,
-  "Books": () => <DisplayResult data={data_books} />,
   "Coding": () => <DisplayResult data={data_code} />,
   "Deployment": () => <DisplayResult data={data_devops} />,
-  "Software Engineering": () => <DisplayResult data={data_swe} />,
+  "Engineering": () => <DisplayResult data={data_swe} />,
+  "Reading": () => <DisplayResult data={data_read} />,
 };
 
 function App() {
   return (
     <>
-      <header className="app-control">  
-        <img className="logo" src={logo}></img>
-      
-      </header>
-        
-      <div className="app-element">
-        <ComponentSwitch cmap={pageMap} />
-      </div>
+      <img className="logo" src={logo}></img>    
+      <ComponentSwitch cmap={pageMap} />
     </>
   )
 }
